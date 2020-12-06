930ba50df9a3bd9817670491bcf0a9bf6d4ace6f
griffinjoshs <leveloutusa@gmail.com>
2020-12-06 12:55:10 -0500

    profile pic onclick

--- assets/tiktok.js
+++ assets/tiktok.js
@@ -20,10 +20,18 @@ $(document).ready(function () {
             // Username
             $('#usernameDisplay').text('@' + username);
             console.log("Username: " + username)
-            
+
             // Proflie Pic
-            $("#profilePic").src = response.avatar_thumb;
-            console.log(response.avatar_thumb)
+            // Set background image of a div on click of the button
+            $('#profilePic').on({
+                'click': function(){
+                    $('#profilePic').attr('src', response.avatar_larger);
+                    console.log(response.avatar_larger)
+                }
+            });
+            // var imageUrl = response.avatar_larger;
+            // $("#profilePic").css("background-image", "url(" + imageUrl + ")");
+            // console.log(response.avatar_larger)
 
             // NickName
             nickname = $("#nickname").text(response.nickname);
